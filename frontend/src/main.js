ef01abb0ddc281af326ac69ce1e09d7af600c68e
Ouzzii <ozankutluok@gmail.com>
2024-10-20 13:25:09 +0300

    deleted unnecessary functions and called Detect_tex_distros function instead

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,30 +5,42 @@ import './style/tree.css';
 import './style/editor.css';
 import './style/preview.css';
 import './style/notification.css';
+import './style/loadingSpinner.css';
+
+import { Detect_tex_distros } from '../wailsjs/go/backend/App';
 
 
 
+function Init() {
+    console.log('creating loading div')
+    var loading = jQuery('<div>', {
+        class: 'loading',
+    })
+    loading.css({
+        'height': '100vh',
+        'width': '100vw',
+        'position': "absolute",
+        'top': '0px',
+        'backgroundColor': 'rgba(27, 38, 54, 1)',
+        'display': 'flex',
+        'flexDirection': 'column'
+    })
+    var loadingSpinner = `
+    <div class="loadingspinner">
+      <div id="square1"></div>
+      <div id="square2"></div>
+      <div id="square3"></div>
+      <div id="square4"></div>
+      <div id="square5"></div>
+    </div>
+    <a>LaTeX Kütüphaneleri kontrol ediliyor</a>
+  `;
+    loading.append(loadingSpinner)
+    $('body').append(loading)
+    Detect_tex_distros().then(function () {
+        loading.remove()
+    })
+    
 
-
-// Setup the greet function
-window.greet = function () {
-    // Get name
-    let name = nameElement.value;
-
-    // Check if the input is empty
-    if (name === "") return;
-
-    // Call App.Greet(name)
-    try {
-        Greet(name)
-            .then((result) => {
-                // Update result with data back from App.Greet()
-                resultElement.innerText = result;
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    } catch (err) {
-        console.error(err);
-    }
-};
+}
+Init()
