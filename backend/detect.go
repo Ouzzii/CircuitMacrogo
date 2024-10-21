085b0eec969db19efa257d0c659028b46f351c47
Ouzzii <ozankutluok@gmail.com>
2024-10-21 23:54:20 +0300

    added function that writes to log file instead of print

--- backend/detect.go
+++ backend/detect.go
@@ -9,8 +9,8 @@ import (
 	"strings"
 )
 
-func (a *App) Detect_tex_distros() {
-
+func (a *App) Detect_tex_distros() Conf {
+	var conf Conf
 	//last distro check
 
 	if runtime.GOOS == "linux" {
@@ -18,10 +18,10 @@ func (a *App) Detect_tex_distros() {
 		detectPdfCmd := exec.Command("sh", "-c", "find / -name miktex-pdflatex 2>&1 | grep -v 'Permission denied' | grep -v 'Invalid argument' | grep -v 'No such file or directory'")
 		output, err := detectPdfCmd.CombinedOutput()
 		if err != nil {
-			Log("Error", fmt.Sprintf("miktex - detect pdflatex komutunu başlatma hatası: %v\n", err))
-			return
+			Log("Error", fmt.Sprintf("miktex - detect pdflatex komutunu başlatma hatası: %v", err))
+			return conf
 		}
-		conf := ReadConf()
+		conf = ReadConf()
 		conf.AddPdflatexPath("miktex", strings.Replace(string(output), "\n", "", 1))
 		conf.WriteConf()
 
@@ -30,8 +30,8 @@ func (a *App) Detect_tex_distros() {
 
 		output, err = detectPdfCmd.CombinedOutput()
 		if err != nil {
-			Log("Error", fmt.Sprintf("texlive - detect pdflatex komutunu başlatma hatası: %v\n", err))
-			return
+			Log("Error", fmt.Sprintf("texlive - detect pdflatex komutunu başlatma hatası: %v", err))
+			return conf
 		}
 
 		for _, pdflatexpath := range strings.Split(string(output), "\n") {
@@ -40,24 +40,25 @@ func (a *App) Detect_tex_distros() {
 				output, err = pdflatexDistroCmd.CombinedOutput()
 				if err != nil {
 					Log("Error", fmt.Sprintf("pdflatex versiyonu kontrol edilirken hata ile karşılaşıldı: %v\n", err))
-					return
+					return conf
 				}
 				if strings.Contains(string(output), "TeX Live") {
-					conf.AddPdflatexPath("tex-live", pdflatexpath)
+					conf.AddPdflatexPath("texlive", pdflatexpath)
 				}
 			}
 		}
 		conf.WriteConf()
-		Boxdims_is_installed()
 
 	} else if runtime.GOOS == "windows" {
 
 	}
+	return conf
+
 }
 
-func Boxdims_is_installed() bool {
+func (a *App) Boxdims_is_installed() Boxdims {
 	conf := ReadConf()
-	boxdims := boxdims{}
+	boxdims := Boxdims{}
 	if runtime.GOOS == "linux" {
 		if _, exists := conf.PdflatexPaths["miktex"]; exists {
 			miktexpath := filepath.Join(os.Getenv("HOME"), ".miktex", "texmfs", "install", "tex", "latex", "circuit_macros")
@@ -68,24 +69,23 @@ func Boxdims_is_installed() bool {
 					//update database
 					exec.Command("sh", "-c", fmt.Sprintf("%v --update-fndb", get_initexmf()))
 					Log("Info", "Miktex: boxdims.sty başarılı bir şekilde indirildi")
-					boxdims.miktex = "exist"
+					boxdims.Miktex = "exist"
 				} else {
 					Log("Error", "Miktex: internet bağlantısı olmadığından boxdims.sty indirilemedi")
-					boxdims.miktex = "not exist"
+					boxdims.Miktex = "not exist"
 				}
 			} else {
 				Log("Info", "Miktex: boxdims.sty zaten kurulu")
-				boxdims.miktex = "exist"
+				boxdims.Miktex = "exist"
 			}
 		} else {
 			Log("Warning", "miktex: cihazda miktex bulunmamaktadır")
 		}
-		if _, exists := conf.PdflatexPaths["tex-live"]; exists {
+		if _, exists := conf.PdflatexPaths["texlive"]; exists {
 			texlivepathCmd := exec.Command("sh", "-c", `find / -wholename "*/texmf-dist/tex/latex" 2>&1 | grep -v "Permission denied" | grep -v "Invalid argument" | grep -v "No such file or directory"`)
 			output, _ := texlivepathCmd.CombinedOutput()
 			texlivePath := strings.Trim(string(output), "\n")
 			if Exists(texlivePath) {
-				fmt.Println(filepath.Join(texlivePath, "circuit_macros.zip"))
 				commandbash := fmt.Sprintf(
 					"wget -P %s https://mirrors.ctan.org/graphics/circuit_macros.zip && unzip %s -d %s && rm %s && mv %s %s && echo successful",
 					texlivePath,
@@ -103,6 +103,7 @@ func Boxdims_is_installed() bool {
 
 						if strings.HasSuffix(result, "successful") {
 							Log("Info", "Texlive için boxdims başarıyla indirildi")
+							boxdims.Texlive = "exist"
 						} else {
 							Log("Error", fmt.Sprintf("Texlive için boxdims indirilirken hata ile karşılaşıldı: %v\n", result))
 						}
@@ -111,6 +112,7 @@ func Boxdims_is_installed() bool {
 					}
 				} else {
 					Log("Info", "texlive: boxdims.sty zaten kurulu")
+					boxdims.Texlive = "exist"
 				}
 			} else {
 				Log("Error", "texlive: Dizin bulunamadı")
@@ -123,12 +125,12 @@ func Boxdims_is_installed() bool {
 
 	}
 
-	return true
+	return boxdims
 }
 
-type boxdims struct {
-	miktex  string
-	texlive string
+type Boxdims struct {
+	Miktex  string `json:"miktex"`
+	Texlive string `json:"texlive"`
 }
 
 func get_initexmf() string {
@@ -146,4 +148,12 @@ func get_initexmf() string {
 		Log("Error", "initexmf komutu bulunamadı")
 	}
 	return initexmf
+}
+
+func (a *App) ChooseDistro(distro string) {
+	conf := ReadConf()
+
+	conf.LastDistro = distro
+	conf.WriteConf()
+
 }
