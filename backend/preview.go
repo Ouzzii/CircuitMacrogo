4d7a4f0faf6366e0d6ade6214f2edc2c1bc6617f
Ouzzii <ozankutluok@gmail.com>
2024-10-21 23:52:06 +0300

    added function that writes to log file instead of print

--- backend/preview.go
+++ backend/preview.go
@@ -2,14 +2,14 @@ package backend
 
 import (
 	"encoding/base64"
+	"fmt"
 	"io/ioutil"
-	"log"
 )
 
 func (a *App) GetPDF(path string) string {
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
-		log.Println("error encountered while reading pdf file:", err)
+		Log("Error", fmt.Sprintf("error encountered while reading pdf file: %v", err))
 		return ""
 	}
 
