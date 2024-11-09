7ebe03b924900926426481c63092cd075561e071
Ouzzii <84467058+Ouzzii@users.noreply.github.com>
2024-11-10 01:20:38 +0300

    the build process is more synchronized

--- frontend/src/scripts/compile.js
+++ frontend/src/scripts/compile.js
@@ -1,14 +1,22 @@
 import { Compile } from "../../wailsjs/go/backend/App"
 
 $('body').on('click', '.compileButton', function(){
-    $('.willcheck:checked').each(function(){
-        generateNotification('info', 'Derleniyor', `${$(this).parent().attr('dir')} derleniyor...`)
-        Compile($(this).parent().find('.compileas').val() ,$(this).parent().attr('dir')).then(function(err){
-            if (err != ""){
-                generateNotification('error', 'Bir hata ile karşılaşıldı', err)
-            }
-            //console.log($(this).parent().find('.compileas').val())
-        })
-    })
+    let compilePromises = [];
 
+    $('.willcheck:checked').each(function() {
+        let compilePromise = Compile($(this).parent().find('.compileas').val(), $(this).parent().attr('dir'))
+            .then(function(err) {
+                if (err != "") {
+                    generateNotification('error', 'Bir hata ile karşılaşıldı', err);
+                }
+            });
+        compilePromises.push(compilePromise);
+        generateNotification('info', 'Derleniyor', `${$(this).parent().attr('dir')} derleniyor...`);
+    });
+
+
+    Promise.all(compilePromises).then(function() {
+
+        window.RefreshAllPreviews(Array.from($(".previewtab")));
+    });
 })
