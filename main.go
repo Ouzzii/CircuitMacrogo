71419be73e2e396dc474032e0dc3c68089f64fc2
Ouzzii <84467058+Ouzzii@users.noreply.github.com>
2024-12-05 23:57:29 +0300

    created handler functions for index file and websocket.

--- main.go
+++ main.go
@@ -1,28 +1,67 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
-	"text/template"
+	"time"
+
+	"github.com/gorilla/websocket"
 )
 
-type User struct {
-	Username string
-	Fname    string
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true // İstekleri doğrulama (geliştirme ortamında kullanılır)
+	},
 }
 
 func main() {
+	// Statik dosyaları serve et
+	fs := http.FileServer(http.Dir("./style"))
+	http.Handle("/style/", http.StripPrefix("/style/", fs))
 
+	// WebSocket handler
+	http.HandleFunc("/ws", handleWebSocket)
+
+	// Ana sayfa handler
 	http.HandleFunc("/", h1)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func h1(w http.ResponseWriter, r *http.Request) {
-	temPlate := template.Must(template.ParseFiles("index.html"))
-	Users := []User{
-		{Username: "Doe", Fname: "John"},
-		{Username: "0zaninyo", Fname: "Ozan"},
-	}
+	temPlate := template.Must(template.ParseFiles("./templates/index.html"))
+	temPlate.Execute(w, nil)
+}
 
-	temPlate.Execute(w, Users)
+// WebSocket bağlantısı için handler
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("WebSocket Bağlantı Hatası:", err)
+		return
+	}
+	defer ws.Close()
+
+	log.Println("WebSocket bağlantısı kuruldu.")
+
+	for {
+
+		_, msg, err := ws.ReadMessage()
+		if err != nil {
+			log.Println("Mesaj Okuma Hatası:", err)
+			break
+		}
+
+		log.Printf("Alınan Mesaj: %s\n", msg)
+		time.Sleep(time.Second * 10)
+
+		// Gelen mesajı tekrar istemciye gönder
+		err = ws.WriteJSON(fmt.Sprintf("Geri Gönderilen: %s", msg))
+		if err != nil {
+			log.Println("Mesaj Gönderme Hatası:", err)
+			break
+		}
+	}
 }
