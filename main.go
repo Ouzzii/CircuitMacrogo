25f612a70cace4eab7a8ec6bd64dedf53743686b
Ouzzii <84467058+Ouzzii@users.noreply.github.com>
2024-12-08 01:47:20 +0300

    removed functions written for testing, and defined all endpoints required for the tree structure within the application

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"time"
+	"webserver/backend"
 
 	"github.com/gorilla/websocket"
 )
@@ -18,11 +17,24 @@ var upgrader = websocket.Upgrader{
 
 func main() {
 	// Statik dosyaları serve et
-	fs := http.FileServer(http.Dir("./style"))
-	http.Handle("/style/", http.StripPrefix("/style/", fs))
+
+	app := backend.App{
+		Configuration: backend.Conf{},
+	}
+
+	app.Configuration = backend.ReadConf()
+
+	styleDir := http.FileServer(http.Dir("./style"))
+	scriptDir := http.FileServer(http.Dir("./scripts"))
+	http.Handle("/style/", http.StripPrefix("/style/", styleDir))
+	http.Handle("/scripts/", http.StripPrefix("/scripts/", scriptDir))
 
 	// WebSocket handler
-	http.HandleFunc("/ws", handleWebSocket)
+	http.HandleFunc("/CheckWorkspace", app.CheckWorkspace)
+	http.HandleFunc("/AskDirectory", app.AskDirectory)
+	http.HandleFunc("/GetDirectory", app.GetDirectory)
+	http.HandleFunc("/IsFile", app.IsFile)
+	http.HandleFunc("/RunDirectoryCheck", app.RunCheckDirectory)
 
 	// Ana sayfa handler
 	http.HandleFunc("/", h1)
@@ -33,35 +45,4 @@ func main() {
 func h1(w http.ResponseWriter, r *http.Request) {
 	temPlate := template.Must(template.ParseFiles("./templates/index.html"))
 	temPlate.Execute(w, nil)
-}
-
-// WebSocket bağlantısı için handler
-func handleWebSocket(w http.ResponseWriter, r *http.Request) {
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println("WebSocket Bağlantı Hatası:", err)
-		return
-	}
-	defer ws.Close()
-
-	log.Println("WebSocket bağlantısı kuruldu.")
-
-	for {
-
-		_, msg, err := ws.ReadMessage()
-		if err != nil {
-			log.Println("Mesaj Okuma Hatası:", err)
-			break
-		}
-
-		log.Printf("Alınan Mesaj: %s\n", msg)
-		time.Sleep(time.Second * 10)
-
-		// Gelen mesajı tekrar istemciye gönder
-		err = ws.WriteJSON(fmt.Sprintf("Geri Gönderilen: %s", msg))
-		if err != nil {
-			log.Println("Mesaj Gönderme Hatası:", err)
-			break
-		}
-	}
 }
