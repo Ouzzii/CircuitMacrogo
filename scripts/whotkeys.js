42f1b34a1978224731537e3ea2a0342a43470aa1
Ouzzii <84467058+Ouzzii@users.noreply.github.com>
2024-12-30 10:53:20 +0300

    hotkeys.js rewritten as whotkeys.js

---
+++ scripts/whotkeys.js
@@ -0,0 +1,27 @@
+//import { SaveContent } from "../../wailsjs/go/backend/App"
+
+
+document.addEventListener('keydown', function(event){
+    console.log("key pressed")
+    if (event.ctrlKey && event.key === 's'){
+        
+        event.preventDefault()
+
+        const path = $('.editTextArea#active').attr('dir')
+        const content = $('.editTextArea#active').val()
+
+
+        fetch("/SaveContent", {
+            method: "POST",
+            body: JSON.stringify({
+                path: path,
+                content: content
+            })
+        }).then(response => response.text()).then(response=>{
+            if (response == "ok"){
+                console.log("Kaydedildi")
+                //generateNotification("success", "Dosya Kaydedildi", `Dosya başarıyla kaydedildi.\nKaydedilen Dosya: ${path}`)
+            }
+        })
+    }
+})
